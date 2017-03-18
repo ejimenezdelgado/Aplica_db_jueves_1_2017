3d7aa047a9106e95d5a5540334b0e8f69bd33643
Efren Jimenez <efjimenez@itcr.ac.cr>
2017-03-17 18:06:20 -0600

    clase 8

--- Clase 8/banco.sql
+++ Clase 8/banco.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE db_banco;
+﻿CREATE DATABASE db_banco;
 
 
 CREATE TABLE cuenta_corriente(id_cliente serial NOT NULL,
@@ -17,3 +17,44 @@ CREATE TABLE bitacora(id_bitacora serial NOT NULL,
                                          tiempo TIME WITH TIME ZONE,
                                                                descripcion varchar(150),
                                                                            CONSTRAINT pk_log PRIMARY KEY (id_bitacora));
+
+
+-------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION transaccion() RETURNS TRIGGER
+AS
+$$
+  DECLARE nombre_tabla VARCHAR(20);
+BEGIN
+  nombre_tabla := TG_TABLE_NAME;
+  RAISE NOTICE 'Trigger llamado en % antes de un %',TG_TABLE_NAME, TG_OP;
+  IF (TG_OP = 'INSERT') THEN
+	INSERT INTO bitacora(tiempo,descripcion)
+	VALUES (NOW(),'Nuevo cliente agregado a la tabla: '||nombre_tabla);
+	RETURN NEW;
+  END IF;
+
+  IF (TG_OP = 'UPDATE') THEN
+     IF (NEW.monto <0) THEN 
+         RAISE NOTICE 'Monto invalido,no se aceptan negativos';
+     END IF;
+     IF (NEW.monto !=OLD.monto) THEN
+         INSERT INTO bitacora(tiempo,descripcion)
+	 VALUES (NOW(),'Monto de la cuenta actulizado tabla:'|| 
+                 nombre_tabla ||' '||'Cliente: '||OLD.nombre_cliente ||
+                 'Monto anterior: '|| OLD.monto || 'Monto nuevo: ' ||
+                 NEW.monto);
+	 RETURN NEW;
+     END IF;		 
+  END IF;
+END
+$$
+LANGUAGE 'plpgsql'
+
+CREATE TRIGGER tr_cuenta_corriente
+BEFORE INSERT OR UPDATE OR DELETE 
+ON cuenta_corriente
+FOR EACH ROW
+EXECUTE PROCEDURE transaccion();
+
+--Prueba 1
+INSERT INTO cuenta_corriente VALUES ('2','Maria Jose Salas', 1000);
